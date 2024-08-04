0cf2240de0ad1bfe0aca83017281bf61f486d595
Sundeep <sundeep.kaler@gmail.com>
2024-08-04 18:33:51 -0400

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm Sundeep.</h1>
+    <img 
+      src={"https://media.licdn.com/dms/image/C4D03AQFILbfAhUAh5Q/profile-displayphoto-shrink_800_800/0/1635195049363?e=1728518400&v=beta&t=zuBLSU87_PefB-ygAM9l9kxX8D7GwWgr476XbKru-uk"}
+      className="profile-pic" 
+      alt="profile pic"
+    />
+    <h2>About Me</h2>
+    <p>This is a paragraphy with my biography.</p>
+    <div>
+        <a
+        className="App-link"
+        href="https://www.linkedin.com/in/sundeep-kaler/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        LinkedIn
+      </a>
+    </div>
+    <div>
+      <a
+        className="App-link"
+        href="https://github.com/s-kaler"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+    </div>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,85 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+test("displays an image with alt text", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const img = screen.getByAltText("profile pic");
+
+    // Assert
+    expect(img).toBeInTheDocument();
+});
+
+
+test("displays a second-level heading with the text `About Me`", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: false,
+        level: 2,
+    });
+
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+});
+
+/*
+test("displays a paragraph with a biography", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const secondLevelHeading = screen.getByRole("paragraph");
+
+    // Assert
+    expect(secondLevelHeading).toBeInTheDocument();
+});
+*/
+
+test("displays a link that contains linkedin url", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const linkedInLink = screen.getByRole("link", {
+        name: /linkedin/i
+    });
+
+    // Assert
+    expect(linkedInLink).toBeInTheDocument();
+    expect(linkedInLink).toHaveAttribute("href", expect.stringContaining('linkedin'));
+});
+
+test("displays a link that contains github url", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+    const githubLink = screen.getByRole("link", {
+        name: /github/i,
+    });
+
+    // Assert
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute("href", expect.stringContaining('github'));
+});
